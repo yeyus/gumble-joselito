a189c0721b73983635761267760d4f79ac242efc
yeyus <elyeyus@gmail.com>
2025-03-20 16:41:14 -0700

    call definition

---
+++ pkg/joselito/call.go
@@ -0,0 +1,48 @@
+package joselito
+
+import (
+	"fmt"
+	"time"
+)
+
+type Call struct {
+	Origin      *DMRID
+	Destination *DMRID
+
+	TalkerAlias string
+	Volume      float32
+
+	Start time.Time
+	End   time.Time
+}
+
+func NewCall(origin *DMRID, destination *DMRID) *Call {
+	return &Call{
+		Origin:      origin,
+		Destination: destination,
+		TalkerAlias: "",
+		Volume:      0,
+		Start:       time.Now(),
+	}
+}
+
+func (c *Call) Finished() bool {
+	return !c.End.IsZero()
+}
+
+func (c *Call) Finish() {
+	c.End = time.Now()
+}
+
+func (c *Call) Duration() time.Duration {
+	end := c.End
+	if end.IsZero() {
+		end = time.Now()
+	}
+
+	return end.Sub(c.Start)
+}
+
+func (c *Call) String() string {
+	return fmt.Sprintf("[Call de:%d to:%d duration:%s talkeralias=%s]", c.Origin.Id, c.Destination.Id, c.Duration().String(), c.TalkerAlias)
+}
